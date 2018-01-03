d7093308c0859cbc2d20e87f07e8a56dc7a69933
Jordan Liu <liu.jordan.com@gmail.com>
2018-01-02 19:52:11 -0800

    add heroku to allowed host

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'kd3=)b!=zu2v#n7_%+qb3he0ng#@%&js_6ra+cn#pr7(-u140w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'sturdy-umbrella.herokuapp.com',
+]
 
 
 # Application definition
